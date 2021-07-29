eb8cb749ad35c694c282df241ef856e8153a526d
anmol92verma <anmol.verma4@gmail.com>
2021-07-29 19:11:39 +0530

    update: compatible and version updates for arctic fox

--- app/src/main/java/com/mutualmobile/tweetify/MainActivity.kt
+++ app/src/main/java/com/mutualmobile/tweetify/MainActivity.kt
@@ -3,12 +3,13 @@ package com.mutualmobile.tweetify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.mutualmobile.tweetify.ui.theme.TweetifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
